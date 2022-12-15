276f4f68e5ec06bb3a57ccd47aff43a3f0e6d87f
Marc Tardif <marc@interunion.ca>
2022-12-15 17:15:09 -0500

    Solve day 13, part 1

---
+++ adventofcode/year2022/day13.py
@@ -0,0 +1,77 @@
+"""Day 13."""
+
+from typing import Iterable, Union
+
+
+Packet = list[Union[int, 'Packet']]
+Pair = tuple[Packet, Packet]
+
+
+def compare(left: Packet, right: Packet) -> bool | None:
+    for i in range(max(len(left), len(right))):
+        # If the left list runs out of items first,
+        # the inputs are in the right order.
+        try:
+            lvalue = left[i]
+        except IndexError:
+            return True
+
+        # If the right list runs out of items first,
+        # the inputs are not in the right order.
+        try:
+            rvalue = right[i]
+        except IndexError:
+            return False
+
+        # If both values are integers,
+        # the lower integer should come first.
+        if isinstance(lvalue, int) and isinstance(rvalue, int):
+            # If the left integer is lower than the right integer,
+            # the inputs are in the right order.
+            if lvalue < rvalue:
+                return True
+            # If the left integer is higher than the right integer,
+            # the inputs are not in the right order.
+            elif lvalue > rvalue:
+                return False
+            # Otherwise, the inputs are the same integer;
+            # continue checking the next part of the input.
+            else:
+                continue
+
+        # If exactly one value is an integer,
+        # convert the integer to a list which contains that
+        # integer as its only value...
+        if isinstance(lvalue, int):
+            lvalue = [lvalue]
+        if isinstance(rvalue, int):
+            rvalue = [rvalue]
+
+        # then retry the comparison.
+        result = compare(lvalue, rvalue)
+        if result is not None:
+            return result
+
+    return None
+
+
+def parse_packet(line: str) -> Packet:
+    return eval(line)
+
+
+def parse_pair(block: str) -> Pair:
+    return tuple(map(parse_packet, block.splitlines()))  # type: ignore
+
+
+def parse_data(data: str) -> Iterable[Pair]:
+    return map(parse_pair, data.split('\n\n'))
+
+
+def part1(data: str) -> int:
+    pairs = parse_data(data)
+    return sum(i for i, pair in enumerate(pairs, 1) if compare(*pair))
+
+
+def part2(data: str) -> int:
+    parse_data(data)
+    return 0
---
+++ tests/year2022/test_day13.py
@@ -0,0 +1,105 @@
+"""Unit tests for year 2022, day 13."""
+
+from textwrap import dedent
+
+import pytest
+
+from adventofcode.year2022.day13 import (
+    compare,
+    parse_data,
+    parse_packet,
+    parse_pair,
+    part1,
+    part2,
+)
+
+
+@pytest.mark.parametrize(
+    'left, right, expected',
+    [
+        ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1], True),
+        ([[1], [2, 3, 4]], [[1], 4], True),
+        ([9], [[8, 7, 6]], False),
+        ([[4, 4], 4, 4], [[4, 4], 4, 4, 4], True),
+        ([7, 7, 7, 7], [7, 7, 7], False),
+        ([], [3], True),
+        ([[[]]], [[]], False),
+        (
+            [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+            [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+            False,
+        ),
+    ],
+)
+def test_compare(left, right, expected):
+    result = compare(left, right)
+    assert result is expected
+
+
+@pytest.mark.parametrize(
+    'packet, expected',
+    [
+        ('[1,1,3,1,1]', [1, 1, 3, 1, 1]),
+        ('[[1],[2,3,4]]', [[1], [2, 3, 4]]),
+        ('[1,[2,[3,[4,[5,6,7]]]],8,9]', [1, [2, [3, [4, [5, 6, 7]]]], 8, 9]),
+    ],
+)
+def test_parse_packet(packet, expected):
+    result = parse_packet(packet)
+    assert result == expected
+
+
+def test_parse_pair():
+    result = parse_pair(
+        dedent(
+            """\
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+        """
+        )
+    )
+    assert result == ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])
+
+
+def test_parse_data():
+    result = parse_data(DATA)
+    assert len(list(result)) == 8
+
+
+def test_part1():
+    result = part1(DATA)
+    assert result == 13
+
+
+def test_part2():
+    result = part2(DATA)
+    assert result == 0
+
+
+DATA = dedent(
+    """\
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
+    """
+)
