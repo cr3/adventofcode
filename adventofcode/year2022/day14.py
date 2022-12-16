ccfbd77182825605cd801e3d69b20cd66e418c3b
Marc Tardif <marc@interunion.ca>
2022-12-16 08:52:50 -0500

    Solve day 14, part 1

---
+++ adventofcode/year2022/day14.py
@@ -0,0 +1,90 @@
+"""Day 14."""
+
+from functools import partial
+from itertools import chain, pairwise, repeat, starmap, takewhile
+from operator import attrgetter, truth
+from typing import Iterable, Type
+
+import attr
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Rock:
+    x: int
+    y: int
+
+    @classmethod
+    def from_string(cls: Type['Rock'], string: str) -> 'Rock':
+        return cls(*map(int, string.split(',')))  # type: ignore
+
+    def with_y(self, y: int) -> 'Rock':
+        return attr.evolve(self, y=y)
+
+    def with_x(self, x: int) -> 'Rock':
+        return attr.evolve(self, x=x)
+
+    def step(self, rocks: set['Rock']) -> 'Rock':
+        # If the tile immediately below is blocked (by rock or sand), the
+        # unit of sand attempts to instead move diagonally one step
+        # down and to the left. If that tile is blocked, the unit of
+        # sand attempts to instead move diagonally one step down and to
+        # the right.
+        for other in [
+            attr.evolve(self, y=self.y + 1),
+            attr.evolve(self, x=self.x - 1, y=self.y + 1),
+            attr.evolve(self, x=self.x + 1, y=self.y + 1),
+        ]:
+            if other not in rocks:
+                return other
+
+        return self
+
+
+Rocks = set['Rock']
+
+
+def drop_rock(rock: Rock, rocks: Rocks):
+    bottom = max(map(attrgetter('y'), rocks))
+    while True:
+        current = rock.step(rocks)
+        if current == rock:
+            rocks.add(current)
+            return True
+
+        if current.y == bottom:
+            return False
+
+        rock = current
+
+
+def expand_rocks(start: Rock, end: Rock) -> Iterable[Rock]:
+    if start.x == end.x:
+        step = 1 if start.y < end.y else -1
+        yield from map(start.with_y, range(start.y, end.y + step, step))
+    else:
+        step = 1 if start.x < end.x else -1
+        yield from map(start.with_x, range(start.x, end.x + step, step))
+
+
+def parse_rocks(line: str) -> Iterable[Rock]:
+    return chain.from_iterable(
+        starmap(
+            expand_rocks,
+            pairwise(map(Rock.from_string, line.split(' -> '))),
+        )
+    )
+
+
+def parse_data(data: str) -> Rocks:
+    return set(chain.from_iterable(map(parse_rocks, data.splitlines())))
+
+
+def part1(data: str) -> int:
+    rocks = parse_data(data)
+    drop = partial(drop_rock, rocks=rocks)
+    return sum(takewhile(truth, map(drop, repeat(Rock(500, 0)))))
+
+
+def part2(data: str) -> int:
+    parse_data(data)
+    return 0
---
+++ tests/year2022/test_day14.py
@@ -0,0 +1,118 @@
+"""Unit tests for year 2022, day 14."""
+
+from textwrap import dedent
+
+import pytest
+
+from adventofcode.year2022.day14 import (
+    Rock,
+    drop_rock,
+    expand_rocks,
+    parse_rocks,
+    part1,
+    part2,
+)
+
+
+@pytest.mark.parametrize(
+    'string, expected',
+    [
+        ('498,4', Rock(498, 4)),
+        ('504,4', Rock(504, 4)),
+    ],
+)
+def test_rock_from_string(string, expected):
+    result = Rock.from_string(string)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'rock, rocks, expected',
+    [
+        (Rock(500, 0), set(), Rock(500, 1)),
+        (Rock(500, 0), set([Rock(500, 1)]), Rock(499, 1)),
+        (Rock(500, 0), set([Rock(500, 1), Rock(499, 1)]), Rock(501, 1)),
+        (
+            Rock(500, 0),
+            set([Rock(500, 1), Rock(499, 1), Rock(501, 1)]),
+            Rock(500, 0),
+        ),
+    ],
+)
+def test_rock_step(rock, rocks, expected):
+    result = rock.step(rocks)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'rock, rocks, expected',
+    [
+        (Rock(500, 0), set([Rock(500, 1)]), False),
+    ],
+)
+def test_drop_rock(rock, rocks, expected):
+    result = drop_rock(rock, rocks)
+    assert result is expected
+
+
+@pytest.mark.parametrize(
+    'start, end, expected',
+    [
+        (
+            Rock(498, 4),
+            Rock(498, 6),
+            [Rock(498, 4), Rock(498, 5), Rock(498, 6)],
+        ),
+        (
+            Rock(498, 6),
+            Rock(496, 6),
+            [Rock(498, 6), Rock(497, 6), Rock(496, 6)],
+        ),
+    ],
+)
+def test_expand_rocks(start, end, expected):
+    result = list(expand_rocks(start, end))
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        (
+            '498,4 -> 498,6',
+            [Rock(498, 4), Rock(498, 5), Rock(498, 6)],
+        ),
+        (
+            '498,4 -> 498,6 -> 496,6',
+            [
+                Rock(498, 4),
+                Rock(498, 5),
+                Rock(498, 6),
+                Rock(498, 6),
+                Rock(497, 6),
+                Rock(496, 6),
+            ],
+        ),
+    ],
+)
+def test_parse_rocks(line, expected):
+    result = list(parse_rocks(line))
+    assert result == expected
+
+
+def test_part1():
+    result = part1(DATA)
+    assert result == 24
+
+
+def test_part2():
+    result = part2(DATA)
+    assert result == 0
+
+
+DATA = dedent(
+    """\
+    498,4 -> 498,6 -> 496,6
+    503,4 -> 502,4 -> 502,9 -> 494,9
+    """
+)
