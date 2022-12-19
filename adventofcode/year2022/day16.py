81a1fdefbed7a1fd0b212394205b28d362f69115
Marc Tardif <marc@interunion.ca>
2022-12-18 21:51:30 -0500

    Solve day 16, part 1

---
+++ adventofcode/year2022/day16.py
@@ -0,0 +1,93 @@
+"""Day 16."""
+
+import re
+from collections import UserDict
+from functools import cached_property
+from itertools import permutations
+from math import inf
+from typing import Iterable
+
+import attr
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Valve:
+    name: str
+    rate: int
+    tunnels: list[str]
+
+    @classmethod
+    def from_line(cls: type['Valve'], line: str):
+        match = re.match(
+            r'Valve (?P<name>[A-Z]{2}) has flow rate=(?P<rate>\d+); '
+            r'tunnels? leads? to valves? (?P<tunnels>.+)',
+            line,
+        )
+        assert match
+        return cls(
+            match['name'],
+            int(match['rate']),
+            match['tunnels'].split(', '),
+        )
+
+
+Answer = dict[int, int]
+
+
+class Graph(UserDict):
+    @classmethod
+    def from_valves(cls: type['Graph'], valves: Iterable[Valve]):
+        return cls({v.name: v for v in valves})
+
+    @cached_property
+    def steps(self):
+        s = {
+            (x, y): 1 if y in self[x].tunnels else inf
+            for x, y in permutations(self, 2)
+        }
+        for k, x, y in permutations(self, 3):
+            s[x, y] = min(s[x, y], s[x, k] + s[k, y])
+
+        return s
+
+    @cached_property
+    def states(self):
+        return {name: 1 << i for i, name in enumerate(self)}
+
+    def travel(
+        self,
+        parent: str,
+        minutes: int,
+        state: int,
+        rate: int,
+        answer: Answer,
+    ) -> Answer:
+        answer[state] = max(answer.get(state, 0), rate)
+        for name, valve in self.items():
+            if valve.rate and parent != name:
+                remaining = minutes - self.steps[parent, name] - 1
+                if not (self.states[name] & state) and (remaining > 0):
+                    self.travel(
+                        name,
+                        remaining,
+                        self.states[name] | state,
+                        rate + remaining * valve.rate,
+                        answer,
+                    )
+
+        return answer
+
+
+def parse_data(data: str) -> Graph:
+    return Graph.from_valves(map(Valve.from_line, data.splitlines()))
+
+
+def part1(data: str) -> int:
+    graph = parse_data(data)
+    answer = graph.travel('AA', 30, 0, 0, {})
+    return max(answer.values())
+
+
+def part2(data: str) -> int:
+    parse_data(data)
+    return 0
---
+++ tests/year2022/test_day16.py
@@ -0,0 +1,130 @@
+"""Unit tests for year 2022, day 16."""
+
+from textwrap import dedent
+
+import pytest
+
+from adventofcode.year2022.day16 import (
+    Graph,
+    Valve,
+    parse_data,
+    part1,
+    part2,
+)
+
+
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        (
+            'Valve AA has flow rate=0; tunnels lead to valves DD, II, BB',
+            Valve('AA', 0, ['DD', 'II', 'BB']),
+        ),
+        (
+            'Valve JJ has flow rate=21; tunnel leads to valve II',
+            Valve('JJ', 21, ['II']),
+        ),
+    ],
+)
+def test_valve_from_line(line, expected):
+    result = Valve.from_line(line)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'valves, steps',
+    [
+        (
+            [
+                Valve('AA', 0, ['BB']),
+                Valve('BB', 1, ['AA']),
+            ],
+            {
+                ('AA', 'BB'): 1,
+                ('BB', 'AA'): 1,
+            },
+        ),
+        (
+            [
+                Valve('AA', 0, ['BB']),
+                Valve('BB', 1, ['CC']),
+                Valve('CC', 2, ['AA']),
+            ],
+            {
+                ('AA', 'BB'): 1,
+                ('AA', 'CC'): 2,
+                ('BB', 'AA'): 2,
+                ('BB', 'CC'): 1,
+                ('CC', 'AA'): 1,
+                ('CC', 'BB'): 2,
+            },
+        ),
+    ],
+)
+def test_graph_steps(valves, steps):
+    graph = Graph.from_valves(valves)
+    assert graph.steps == steps
+
+
+@pytest.mark.parametrize(
+    'valves, answer',
+    [
+        (
+            [
+                Valve('AA', 0, ['BB']),
+                Valve('BB', 1, ['AA']),
+            ],
+            {
+                0: 0,
+                2: 28,
+            },
+        ),
+        (
+            [
+                Valve('AA', 0, ['BB']),
+                Valve('BB', 1, ['CC']),
+                Valve('CC', 2, ['AA']),
+            ],
+            {
+                0: 0,
+                2: 28,
+                4: 54,
+                6: 80,
+            },
+        ),
+    ],
+)
+def test_graph_travel(valves, answer):
+    graph = Graph.from_valves(valves)
+    assert graph.travel('AA', 30, 0, 0, {}) == answer
+
+
+def test_parse_data():
+    graph = parse_data(DATA)
+    assert len(graph) == 10
+
+
+def test_part1():
+    result = part1(DATA)
+    assert result == 1651
+
+
+def test_part2():
+    result = part2(DATA)
+    assert result == 0
+
+
+DATA = dedent(
+    """\
+    Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    Valve BB has flow rate=13; tunnels lead to valves CC, AA
+    Valve CC has flow rate=2; tunnels lead to valves DD, BB
+    Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+    Valve EE has flow rate=3; tunnels lead to valves FF, DD
+    Valve FF has flow rate=0; tunnels lead to valves EE, GG
+    Valve GG has flow rate=0; tunnels lead to valves FF, HH
+    Valve HH has flow rate=22; tunnel leads to valve GG
+    Valve II has flow rate=0; tunnels lead to valves AA, JJ
+    Valve JJ has flow rate=21; tunnel leads to valve II
+    """
+)
