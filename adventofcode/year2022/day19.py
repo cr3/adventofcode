adcaeb15a2a37ed4b125cf2993c0da4ae18f816b
Marc Tardif <marc@interunion.ca>
2022-12-22 18:39:40 -0500

    Solve day 19, part 1

---
+++ adventofcode/year2022/day19.py
@@ -0,0 +1,171 @@
+"""Day 19.
+
+https://nirlipo.github.io/Width-Based-Planning-Resources/
+"""
+
+import re
+from itertools import combinations
+from typing import Iterable, Literal, Optional
+
+import attr
+
+
+Material = Literal['ore', 'clay', 'obsidian', 'geode']
+Bots = dict[Material, int]
+Resources = dict[Material, int]
+Costs = dict[Material, Resources]
+
+MATERIALS: list[Material] = ['ore', 'clay', 'obsidian', 'geode']
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class State:
+    minutes: int
+    bots: Bots = attr.ib(factory=dict)
+    resources: Resources = attr.ib(factory=dict)
+    building: Optional[Material] = None
+
+    @property
+    def atoms(self) -> Iterable[str]:
+        atoms = [
+            f'minutes={self.minutes}',
+            *[f'{m}={self.resources.get(m, 0)}' for m in MATERIALS],
+            *[f'bot-{m}={self.bots.get(m, 0)}' for m in MATERIALS],
+        ]
+        if self.building:
+            atoms.append(f'bot-{self.building}=1')
+
+        return atoms
+
+    def tick(self) -> 'State':
+        minutes = self.minutes - 1
+        resources = {
+            m: self.resources.get(m, 0) + self.bots.get(m, 0)
+            for m in MATERIALS
+        }
+        bots = self.bots.copy()
+        if self.building:
+            bots[self.building] = bots.get(self.building, 0) + 1
+
+        return attr.evolve(
+            self,
+            minutes=minutes,
+            bots=bots,
+            resources=resources,
+            building=None,
+        )
+
+    def buy(self, name: Material, costs: Costs) -> Optional['State']:
+        cost = costs.get(name, {})
+        minutes_limit = cost.get('ore', 0) + 1 if name == 'ore' else 1
+        max_cost = (
+            float('inf')
+            if name == 'geode'
+            else max(c.get(name, 0) for c in costs.values())
+        )
+        if (
+            self.minutes <= minutes_limit
+            or any(v > self.resources.get(k, 0) for k, v in cost.items())
+            or self.bots.get(name, 0) >= max_cost
+        ):
+            return None
+
+        resources = {
+            **self.resources,
+            **{k: self.resources[k] - cost[k] for k, v in cost.items()},
+        }
+        return attr.evolve(self, resources=resources, building=name)
+
+    def prune(self, atoms: set[int], width: int) -> Optional['State']:
+        prune = True
+        for pairs in combinations(self.atoms, width):
+            atom = hash(pairs)
+            if atom not in atoms:
+                prune = False
+                atoms.add(atom)
+
+        if prune:
+            return None
+
+        return self
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Blueprint:
+    num: int
+    costs: Costs
+
+    @classmethod
+    def from_line(cls: type['Blueprint'], line: str) -> 'Blueprint':
+        (
+            num,
+            ore_ores,
+            clay_ores,
+            obsidian_ores,
+            obsidian_clays,
+            geode_ores,
+            geode_obsidians,
+        ) = map(int, re.findall(r'\d+', line))
+        return cls(
+            num,
+            {
+                'ore': {'ore': ore_ores},
+                'clay': {'ore': clay_ores},
+                'obsidian': {'ore': obsidian_ores, 'clay': obsidian_clays},
+                'geode': {'ore': geode_ores, 'obsidian': geode_obsidians},
+            },
+        )
+
+    def calculate(self, minutes: int, width: int):
+        best = 0
+        atoms: set[int] = set()
+        state = State(
+            minutes,
+            {'ore': 1},
+        )
+        queue = [state]
+        while queue:
+            state = queue.pop(0)
+            if state.minutes == 0:
+                best = max(best, state.resources.get('geode', 0))
+                continue
+
+            next_state = state.tick()
+            materials = reversed(MATERIALS)
+            if buy_state := next_state.buy(next(materials), self.costs):
+                if novel := buy_state.prune(atoms, width):
+                    queue.append(novel)
+            else:
+                for material in materials:
+                    if buy_state := next_state.buy(material, self.costs):
+                        if novel := buy_state.prune(atoms, width):
+                            queue.append(novel)
+
+                if novel := next_state.prune(atoms, width):
+                    queue.append(novel)
+
+        return best
+
+    def evaluate(self, minutes: int) -> int:
+        result = -1
+        for width in range(1, 4):  # pragma: no cover
+            current = self.calculate(minutes, width)
+            if current == result:
+                break
+            result = current
+
+        return result
+
+
+def parse_data(data: str) -> Iterable[Blueprint]:
+    return map(Blueprint.from_line, data.splitlines())
+
+
+def part1(data: str) -> int:
+    blueprints = parse_data(data)
+    return sum(bp.num * bp.evaluate(24) for bp in blueprints)
+
+
+def part2(data: str) -> int:
+    parse_data(data)
+    return 0
---
+++ tests/year2022/test_day19.py
@@ -0,0 +1,110 @@
+"""Unit tests for year 2022, day 19."""
+
+from textwrap import dedent
+
+import pytest
+
+from adventofcode.year2022.day19 import (
+    Blueprint,
+    State,
+    part1,
+    part2,
+)
+
+
+@pytest.mark.parametrize(
+    'state, costs, expected',
+    [
+        (
+            State(2, {}, {}),
+            {'ore': {'ore': 1}},
+            None,
+        ),
+        (
+            State(3, {}, {'ore': 1}),
+            {'ore': {'ore': 1}},
+            State(3, {}, {'ore': 0}, building='ore'),
+        ),
+    ],
+)
+def test_state_buy(state, costs, expected):
+    result = state.buy('ore', costs)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        (
+            dedent(
+                """\
+                Blueprint 1: \
+                Each ore robot costs 4 ore. \
+                Each clay robot costs 2 ore. \
+                Each obsidian robot costs 3 ore and 14 clay. \
+                Each geode robot costs 2 ore and 7 obsidian.
+                """
+            ),
+            Blueprint(
+                1,
+                {
+                    'ore': {'ore': 4},
+                    'clay': {'ore': 2},
+                    'obsidian': {'ore': 3, 'clay': 14},
+                    'geode': {'ore': 2, 'obsidian': 7},
+                },
+            ),
+        ),
+        (
+            dedent(
+                """\
+                Blueprint 2: \
+                Each ore robot costs 2 ore. \
+                Each clay robot costs 3 ore. \
+                Each obsidian robot costs 3 ore and 8 clay. \
+                Each geode robot costs 3 ore and 12 obsidian. \
+                """
+            ),
+            Blueprint(
+                2,
+                {
+                    'ore': {'ore': 2},
+                    'clay': {'ore': 3},
+                    'obsidian': {'ore': 3, 'clay': 8},
+                    'geode': {'ore': 3, 'obsidian': 12},
+                },
+            ),
+        ),
+    ],
+)
+def test_blueprint_from_line(line, expected):
+    result = Blueprint.from_line(line)
+    assert result == expected
+
+
+def test_part1():
+    result = part1(DATA)
+    assert result == 33
+
+
+def test_part2():
+    result = part2(DATA)
+    assert result == 0
+
+
+DATA = dedent(
+    """\
+    Blueprint 1:\
+     Each ore robot costs 4 ore.\
+     Each clay robot costs 2 ore.\
+     Each obsidian robot costs 3 ore and 14 clay.\
+     Each geode robot costs 2 ore and 7 obsidian.\
+
+    Blueprint 2:\
+     Each ore robot costs 2 ore.\
+     Each clay robot costs 3 ore.\
+     Each obsidian robot costs 3 ore and 8 clay.\
+     Each geode robot costs 3 ore and 12 obsidian.\
+
+    """,
+)
