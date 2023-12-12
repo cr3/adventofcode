777f5e3452a732b1b2ec67387f5908ff52707da4
Marc Tardif <marc@interunion.ca>
2023-12-11 21:20:28 -0500

    Solve year 2023, day 1, part 2

--- adventofcode/year2023/day01.py
+++ adventofcode/year2023/day01.py
@@ -1,18 +1,37 @@
 """Day 1."""
 
+import re
+from collections.abc import Callable
 
-def parse_line(line: str) -> int:
+DIGIT_NAMES = 'one two three four five six seven eight nine'.split()
+
+
+def parse_digit(digit):
+    try:
+        return DIGIT_NAMES.index(digit) + 1
+    except ValueError:
+        return int(digit)
+
+
+def parse_alphanumeric(line: str) -> int:
+    pattern = rf'(?=(\d|{"|".join(DIGIT_NAMES)}))'
+    matches = re.finditer(pattern, line)
+    digits = [parse_digit(m.group(1)) for m in matches]
+    return digits[0] * 10 + digits[-1]
+
+
+def parse_numeric(line: str) -> int:
     digits = [c for c in line if c.isdigit()]
-    return int(f"{digits[0]}{digits[-1]}")
+    return int(f'{digits[0]}{digits[-1]}')
 
 
-def parse_data(data: str) -> int:
-    return sum(map(parse_line, data.splitlines()))
+def parse_data(data: str, parser: Callable[[str], int]) -> int:
+    return sum(map(parser, data.splitlines()))
 
 
 def part1(data: str) -> int:
-    return parse_data(data)
+    return parse_data(data, parse_numeric)
 
 
 def part2(data: str) -> int:
-    return 0
+    return parse_data(data, parse_alphanumeric)
--- tests/year2023/test_day01.py
+++ tests/year2023/test_day01.py
@@ -1,35 +1,86 @@
-"""Unit tests for year 2023, day 1."""
+'''Unit tests for year 2023, day 1.'''
 
 from textwrap import dedent
 
 import pytest
+
 from adventofcode.year2023.day01 import (
-    parse_line,
+    parse_alphanumeric,
     parse_data,
+    parse_digit,
+    parse_numeric,
     part1,
+    part2,
 )
 
 
-@pytest.mark.parametrize("line, expected", [
-    ("12", 12),
-    ("1b2", 12),
-    ("a12", 12),
-    ("12c", 12),
-    ("a1b2c", 12),
-    ("a1b2c3d", 13),
-])
-def test_parse_line(line, expected):
-    num = parse_line(line)
+@pytest.mark.parametrize(
+    'digit, expected',
+    [
+        ('1', 1),
+        ('2', 2),
+        ('one', 1),
+        ('two', 2),
+        ('three', 3),
+        ('four', 4),
+        ('five', 5),
+        ('six', 6),
+        ('seven', 7),
+        ('eight', 8),
+        ('nine', 9),
+    ],
+)
+def test_parse_digit(digit, expected):
+    num = parse_digit(digit)
     assert num == expected
 
 
-@pytest.mark.parametrize("data, expected", [
-    ("11\n", 11),
-    ("11\n22\n", 33),
-    ("11\n22\n33\n", 66),
-])
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        ('one', 11),
+        ('1two', 12),
+        ('one2', 12),
+        ('onetwo', 12),
+        ('12three', 13),
+        ('1twothree', 13),
+        ('onetwothree', 13),
+        ('onetwo3', 13),
+        ('one2three', 13),
+        ('one23', 13),
+    ],
+)
+def test_parse_alphanumeric(line, expected):
+    num = parse_alphanumeric(line)
+    assert num == expected
+
+
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        ('1', 11),
+        ('12', 12),
+        ('1b2', 12),
+        ('a12', 12),
+        ('12c', 12),
+        ('a1b2c', 12),
+        ('a1b2c3d', 13),
+    ],
+)
+def test_parse_numeric(line, expected):
+    num = parse_numeric(line)
+    assert num == expected
+
+
+@pytest.mark.parametrize(
+    'data, expected',
+    [
+        ('\n', 1),
+        ('\n\n', 2),
+    ],
+)
 def test_parse_data(data, expected):
-    total = parse_data(data)
+    total = parse_data(data, lambda _: 1)
     assert total == expected
 
 
@@ -41,3 +92,16 @@ def test_part1():
         treb7uchet
         '''))
     assert result == 142
+
+
+def test_part2():
+    result = part2(dedent('''\
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        '''))
+    assert result == 281
