ba78636fb1054b73294eeb087942f6c9fa1d6612
Marc Tardif <marc@interunion.ca>
2023-12-13 22:44:27 -0500

    Solve year 2023, day 2, part 1

---
+++ adventofcode/year2023/day02.py
@@ -0,0 +1,57 @@
+"""Day 2."""
+
+import re
+
+from attrs import define
+
+
+@define(frozen=True)
+class Play:
+    blue: int = 0
+    green: int = 0
+    red: int = 0
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Play':
+        kwargs = {
+            m['k']: int(m['v'])
+            for m in re.finditer(r'(?P<v>\d+) (?P<k>\w+)', string)
+        }
+        return cls(**kwargs)
+
+    def __le__(self, other: 'Play') -> bool:
+        return (
+            self.blue <= other.blue
+            and self.green <= other.green
+            and self.red <= other.red
+        )
+
+
+@define(frozen=True)
+class Game:
+    num: int
+    plays: list[Play]
+
+    @classmethod
+    def from_line(cls, line: str) -> 'Game':
+        if m := re.match(r'Game (?P<num>\d+): (?P<plays>.*)', line):
+            plays = [Play.from_string(s) for s in m['plays'].split('; ')]
+            num = int(m['num'])
+            return cls(num, plays)
+
+        raise ValueError(f'Unexpected line: {line}')
+
+    def is_within(self, limit: Play) -> bool:
+        return all(play <= limit for play in self.plays)
+
+
+def part1(data: str) -> int:
+    # 12 red cubes, 13 green cubes, and 14 blue cubes
+    limit = Play(red=12, green=13, blue=14)
+    games = [Game.from_line(line) for line in data.splitlines()]
+    total = sum(g.num for g in games if g.is_within(limit))
+    return total
+
+
+def part2(data: str) -> int:
+    return 0
---
+++ tests/year2023/test_day02.py
@@ -0,0 +1,96 @@
+'''Unit tests for year 2023, day 2.'''
+
+from textwrap import dedent
+
+import pytest
+
+from adventofcode.year2023.day02 import (
+    Game,
+    Play,
+    part1,
+    part2,
+)
+
+
+@pytest.mark.parametrize(
+    'string, expected',
+    [
+        ('1 blue', Play(blue=1)),
+        ('1 green', Play(green=1)),
+        ('1 red', Play(red=1)),
+        ('1 blue, 2 green', Play(blue=1, green=2)),
+        ('1 blue, 2 green, 3 red', Play(blue=1, green=2, red=3)),
+    ],
+)
+def test_play_from_string(string, expected):
+    result = Play.from_string(string)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'play, other, expected',
+    [
+        (Play(), Play(blue=1), True),
+        (Play(blue=1), Play(blue=1), True),
+        (Play(blue=2), Play(blue=1), False),
+    ],
+)
+def test_play_le(play, other, expected):
+    result = play <= other
+    assert result is expected
+
+
+@pytest.mark.parametrize(
+    'line, expected',
+    [
+        (
+            'Game 1: 1 blue',
+            Game(1, [Play(blue=1)]),
+        ),
+        (
+            'Game 2: 1 blue; 1 red',
+            Game(2, [Play(blue=1), Play(red=1)]),
+        ),
+        (
+            'Game 10: 1 blue, 1 red; 2 red',
+            Game(10, [Play(blue=1, red=1), Play(red=2)]),
+        ),
+        (
+            'Game 1: 1 blue; 2 blue, 2 red',
+            Game(1, [Play(blue=1), Play(blue=2, red=2)]),
+        ),
+    ],
+)
+def test_game_from_line(line, expected):
+    result = Game.from_line(line)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'game, other, expected',
+    [
+        (Game(1, [Play()]), Play(blue=1), True),
+        (Game(1, [Play(blue=1)]), Play(blue=1), True),
+        (Game(1, [Play(blue=2)]), Play(blue=1), False),
+        (Game(1, [Play(), Play(blue=2)]), Play(blue=1), False),
+    ],
+)
+def test_game_le(game, other, expected):
+    result = game <= other
+    assert result is expected
+
+
+def test_part1():
+    result = part1(dedent('''\
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        '''))
+    assert result == 8
+
+
+def test_part2():
+    result = part2('')
+    assert result == 0
