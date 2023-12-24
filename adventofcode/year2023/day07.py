d51f41ec39c788075878c31de9286f99045509ef
Marc Tardif <marc@interunion.ca>
2023-12-24 12:06:46 -0500

    Solve year 2023, day 7, part 2

--- adventofcode/year2023/day07.py
+++ adventofcode/year2023/day07.py
@@ -6,8 +6,6 @@ from itertools import groupby
 
 from attrs import define
 
-CARDS = '23456789TJQKA'
-
 
 class HandType(IntEnum):
     FIVE_OF_A_KIND = 6
@@ -21,12 +19,13 @@ class HandType(IntEnum):
 
 @total_ordering
 @define(frozen=True, order=False)
-class Hand:
+class Hand1:
     cards: str
     bid: int = 0
+    labels: str = '23456789TJQKA'
 
     @classmethod
-    def from_string(cls, string: str) -> 'Hand':
+    def from_string(cls, string: str) -> 'Hand1':
         cards, bid = string.split()
         return cls(cards, int(bid))
 
@@ -40,11 +39,11 @@ class Hand:
             return HandType.FIVE_OF_A_KIND
         elif groups[0] == 4:
             return HandType.FOUR_OF_A_KIND
-        elif groups[0] == 3 and groups[1] == 2:
+        elif groups[0] + groups[1] == 5:
             return HandType.FULL_HOUSE
         elif groups[0] == 3:
             return HandType.THREE_OF_A_KIND
-        elif groups[0] == 2 and groups[1] == 2:
+        elif groups[0] + groups[1] == 4:
             return HandType.TWO_PAIRS
         elif groups[0] == 2:
             return HandType.ONE_PAIR
@@ -53,17 +52,55 @@ class Hand:
 
     @property
     def weight(self) -> list[int]:
-        return [self.type_, *map(CARDS.index, self.cards)]
+        return [self.type_, *map(self.labels.index, self.cards)]
 
     def __lt__(self, other) -> bool:
         return self.weight < other.weight
 
 
+@total_ordering
+@define(frozen=True, order=False)
+class Hand2(Hand1):
+    labels: str = 'J23456789TQKA'
+
+    @property
+    def jokers(self):
+        return ''.join(c for c in self.cards if c == 'J')
+
+    @property
+    def non_jokers(self):
+        return ''.join(c for c in self.cards if c != 'J')
+
+    @property
+    def type_(self) -> HandType:
+        njokers = len(self.jokers)
+        groups = sorted(
+            (len(list(g[1])) for g in groupby(sorted(self.non_jokers))),
+            reverse=True,
+        ) or [0]
+        if groups[0] + njokers == 5:
+            return HandType.FIVE_OF_A_KIND
+        elif groups[0] + njokers == 4:
+            return HandType.FOUR_OF_A_KIND
+        elif groups[0] + groups[1] + njokers == 5:
+            return HandType.FULL_HOUSE
+        elif groups[0] + njokers == 3:
+            return HandType.THREE_OF_A_KIND
+        elif groups[0] + groups[1] + njokers == 4:
+            return HandType.TWO_PAIRS
+        elif groups[0] + njokers == 2:
+            return HandType.ONE_PAIR
+        else:
+            return HandType.HIGH_CARD
+
+
 def part1(data: str) -> int:
-    hands = sorted(map(Hand.from_string, data.splitlines()))
+    hands = sorted(map(Hand1.from_string, data.splitlines()))
     total = sum(i * h.bid for i, h in enumerate(hands, start=1))
     return total
 
 
 def part2(data: str) -> int:
-    return 0
+    hands = sorted(map(Hand2.from_string, data.splitlines()))
+    total = sum(i * h.bid for i, h in enumerate(hands, start=1))
+    return total
--- tests/year2023/test_day07.py
+++ tests/year2023/test_day07.py
@@ -5,7 +5,8 @@ from textwrap import dedent
 import pytest
 
 from adventofcode.year2023.day07 import (
-    Hand,
+    Hand1,
+    Hand2,
     HandType,
     part1,
     part2,
@@ -15,12 +16,12 @@ from adventofcode.year2023.day07 import (
 @pytest.mark.parametrize(
     'string, expected',
     [
-        ('12345 0', Hand('12345', 0)),
-        ('AKQJT 10', Hand('AKQJT', 10)),
+        ('12345 0', Hand1('12345', 0)),
+        ('AKQJT 10', Hand1('AKQJT', 10)),
     ],
 )
 def test_hand_from_string(string, expected):
-    result = Hand.from_string(string)
+    result = Hand1.from_string(string)
     assert result == expected
 
 
@@ -38,8 +39,8 @@ def test_hand_from_string(string, expected):
         ('23456', HandType.HIGH_CARD),
     ],
 )
-def test_hand_type(cards, expected):
-    result = Hand(cards).type_
+def test_hand1_type(cards, expected):
+    result = Hand1(cards).type_
     assert result == expected
 
 
@@ -50,8 +51,8 @@ def test_hand_type(cards, expected):
         ('23456', [0, 0, 1, 2, 3, 4]),
     ],
 )
-def test_hand_weight(cards, expected):
-    result = Hand(cards).weight
+def test_hand1_weight(cards, expected):
+    result = Hand1(cards).weight
     assert result == expected
 
 
@@ -65,8 +66,43 @@ def test_hand_weight(cards, expected):
         ('KK677', 'KTJJT'),
     ],
 )
-def test_hand_gt(a, b):
-    assert Hand(a) > Hand(b)
+def test_hand1_gt(a, b):
+    assert Hand1(a) > Hand1(b)
+
+
+@pytest.mark.parametrize(
+    'cards, expected',
+    [
+        ('AAAAA', HandType.FIVE_OF_A_KIND),
+        ('AA8AA', HandType.FOUR_OF_A_KIND),
+        ('23332', HandType.FULL_HOUSE),
+        ('TTT98', HandType.THREE_OF_A_KIND),
+        ('23432', HandType.TWO_PAIRS),
+        ('A23A4', HandType.ONE_PAIR),
+        ('2345A', HandType.HIGH_CARD),
+        ('A3456', HandType.HIGH_CARD),
+        ('23456', HandType.HIGH_CARD),
+        ('T55J5', HandType.FOUR_OF_A_KIND),
+        ('KTJJT', HandType.FOUR_OF_A_KIND),
+        ('QQQJA', HandType.FOUR_OF_A_KIND),
+    ],
+)
+def test_hand2_type(cards, expected):
+    result = Hand2(cards).type_
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'cards, expected',
+    [
+        ('AAAAA', [HandType.FIVE_OF_A_KIND, 12, 12, 12, 12, 12]),
+        ('23456', [HandType.HIGH_CARD, 1, 2, 3, 4, 5]),
+        ('J2222', [HandType.FIVE_OF_A_KIND, 0, 1, 1, 1, 1]),
+    ],
+)
+def test_hand2_weight(cards, expected):
+    result = Hand2(cards).weight
+    assert result == expected
 
 
 def test_part1():
@@ -88,4 +124,4 @@ def test_part2():
         KTJJT 220
         QQQJA 483
         '''))
-    assert result == 0
+    assert result == 5905
